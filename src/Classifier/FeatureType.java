afe6490520b965b1f14d8492a5044764bb74f6b1
chunguo <guochun2003@gmail.com>
2012-10-22 11:37:41 -0400

    Adding FeatureType class

---
+++ src/Classifier/FeatureType.java
@@ -0,0 +1,17 @@
+package Classifier;
+
+import java.util.ArrayList;
+
+public class FeatureType {
+
+	public FeatureType() {
+
+	}
+
+	public String addDaysSinceRelease() {
+		
+		return "DaysSinceRelease";
+	}
+	
+	
+}
